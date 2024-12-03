f816030c51c1b668657ecc7aabe13761cb1432bf
jyneeweenie <jynemarie1@gmail.com>
2024-12-03 12:40:00 +0300

    done

---
+++ Activity-3/main.py
@@ -0,0 +1,13 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+example = "Haa"*5
+print("First_name :", first_name)
+print("Last_name :", last_name)
+print("Full_name :", full_name)
+print("String Multiplied 5 times gives this result :",example)
+word = 'Coding'
+print("Length of String :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[5])
+print("String Sliced :", word[0.3])
---
+++ main.py
@@ -0,0 +1,10 @@
+# Take input values from user
+x =  input("Enter value of x:")
+y =  input("Enter value of y:")
+x = 5
+y = 9
+z = 1
+# Displaying results after swapping
+print("value of x after swapping", x)
+print("value of y after swapping", y)
+print("value of z after swapping", z)
